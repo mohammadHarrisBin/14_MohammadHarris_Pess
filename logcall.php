88d8beabc5a4a367809af3df75cbca703aa7e7cb
mohammadHarris <harrishero2003@gmail.com>
2021-10-14 15:03:12 +0800

    dd

---
+++ logcall.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Police Emergency Service System</title>
+    <link rel="stylesheet" href="./styles/logcall.css">
+</head>
+
+<body>
+    <script>
+        const mohammadHarris = () => {
+            var x = document.forms['frmLogCall']['callerName'].value;
+            if (x == null || x == '') {
+                alert('Caller Name is required');
+                return false;
+            }
+        }
+    </script>
+    <?php
+    include('nav.php');
+    include('db.php');
+    //connecting the database
+    $mysqli= mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
+
+    if ($mysqli->connect_error) {
+        die("Error connecting to the database: " . $mysqli->connect_error);
+    }
+
+    $sql = "SELECT * FROM incidenttype";
+    
+
+    if (!($stmt = $mysqli->prepare($sql))) {
+        die("Command error: " . $mysqli->error);
+    }
+
+    if (!$stmt->execute()) {
+        die('Cannot run SQL command' . $stmt->error);
+    }
+
+    if (!($resultset = $stmt->get_result())) {
+        die('No data is resultset: ' . $stmt->error);
+    }
+
+    $incidentType;
+
+    while ($row = $resultset->fetch_assoc()) {
+        $incidentType[$row['incidentTypeId']] = $row['incidentTypeDesc'];
+    }
+
+    $stmt->close();
+
+    $resultset->close();
+
+    $mysqli->close();
+    
+    ?>
+    
+
+    <div id="container">
+        
+        <form method="post" action='dispatch.php' onSubmit="return mohammadHarris();" name='frmLogCall'>
+
+            <table>
+                <tr>
+                    <h1 class='table__top'>
+                    Log Call Panel
+                </h1></tr>
+                
+                <tr>
+                    <td class='table__left'>Caller's Name:</td>
+                    <td class='table__right'><input type="text" name='callerName' id='callerName' placeholder="callers name"></td>
+                </tr>
+
+                <tr>
+                    <td class='table__left'>Contact No:</td>
+                    <td class='table__right'><input type="text" name='contactNo' id='contactNo' placeholder="e.g 98651234"></td>
+                </tr>
+
+                <tr>
+                    <td class='table__left'>Location:</td>
+                    <td class='table__right'><input type="text" name='location' id='location' placeholder='e.g Singapore'></td>
+                </tr>
+
+                <tr>
+                    <td class='table__left'>Incident Type</td>
+                    <td class='table__right'>
+                        <select name='incidentType' id='incidentType'>
+                            <?php
+                            echo $incidentType[0];
+                            foreach($incidentType as $incident){
+                                echo '<option value="'.$incident.'".>'.$incident.'</option>';
+                            }
+                            ?>
+                        </select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td class='table__left'>Description:</td>
+                    <td class='table__right'>
+                        <Textarea name='incidentDesc' id='incidentDesc' cols='45' placeholder='Accident etc...'></Textarea>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td class='table__left'>
+                        <button type="reset" name='btnCancel'>Reset</button>
+                    </td>
+                    <td class='table__right'>
+                        <button type="submit" name='btnProcessCall' id='btnProcessCall'>Process Call</button>
+                    </td>
+                </tr>
+
+
+            </table>
+
+        </form>
+        
+    </div>
+
+</body>
+
+</html>
---
+++ nav.php
@@ -0,0 +1,22 @@
+ <!DOCTYPE html>
+ <html lang="en">
+
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Police Emergency Service System</title>
+     <link rel="stylesheet" href="./styles/nav.css">
+ </head>
+
+ <body>
+     <img src="15_MohammadHarris_pessBanner.jpg" alt="banner" width='960' height="160">
+     <nav>
+         <a href="logcall.php">Log Call</a>
+         <a href="Update.php">Update</a>
+         <a href="#">Report</a>
+         <a href="#">History</a>
+     </nav>
+ </body>
+
+ </html>
